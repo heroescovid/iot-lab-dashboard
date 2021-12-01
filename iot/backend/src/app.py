b9b0eef24f040e360cc1bbabad0ec6576d50c9b8
iot-lab <iot.rc.systems@gmail.com>
2021-11-30 18:30:53 -0600

    iot-lab commit

--- iot/backend/src/app.py
+++ iot/backend/src/app.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template
 import paho as mqtt
 app = Flask(__name__)
 @app.route("/")
-def holamundo():
+def index():
     return render_template("index.html")
+@app.route("/sensores")
+def grafica():
+    return render_template("grafica.html")
 app.run(host = "0.0.0.0",port = 5050,debug = True)
